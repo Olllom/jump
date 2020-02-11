ac00d4c4245e73c168dc52d7c71c18def6b58479
Andreas Krämer <kraemer.research@gmail.com>
2020-02-11 14:09:59 +0100

    empty tests

--- tests/test_jump.py
+++ tests/test_jump.py
@@ -3,9 +3,9 @@
 
 """Tests for `jump` package."""
 
-import pytest
+#import pytest
 
-from click.testing import CliRunner
+#from click.testing import CliRunner
 
 #from jump.jump import jump
 #from jump.jump import cli
